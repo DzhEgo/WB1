4d790b674b4eaa16a0528e8263686391adc0ad9f
DzhEgo <maximalkin2005@gmail.com>
2024-01-20 17:12:10 +0300

    descr

--- 21.go
+++ 21.go
@@ -2,10 +2,17 @@ package main
 
 import "fmt"
 
+// Создаем интерфейс Adaptee, который несовместим с существующим клиентским кодом
 type Adaptee struct{}
 
+// Создаем Адаптер, который делает интерфейс Adaptee совместимым с Target
 type Adapter struct {
 	adaptee *Adaptee
+}
+
+// Создаем интерфейс Target, который использует клиент
+type Target interface {
+	Request() string
 }
 
 func (a *Adaptee) SpecRequest() string {
@@ -20,10 +27,7 @@ func (a *Adapter) Request() string {
 	return a.adaptee.SpecRequest()
 }
 
-type Target interface {
-	Request() string
-}
-
+// Создаем клиентский код, который поддерживает все классы, которые следуют Target
 func ClientCode(target Target) {
 	fmt.Println("Client: Я могу работать с целевыми объектами:", target.Request())
 }
